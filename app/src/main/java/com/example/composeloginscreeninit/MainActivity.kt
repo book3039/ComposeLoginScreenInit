c8a17a864dd063f7cdd0db21582cd3e5827458a3
book3039@knou.ac.kr <book3039@knou.ac.kr>
2024-09-14 01:21:16 +0900

    Add LoginScreen

--- app/src/main/java/com/example/composeloginscreeninit/MainActivity.kt
+++ app/src/main/java/com/example/composeloginscreeninit/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.composeloginscreeninit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.example.composeloginscreeninit.ui.theme.ComposeLoginScreenInitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ComposeLoginScreenInitTheme {
-
+                LoginScreen()
             }
         }
     }
